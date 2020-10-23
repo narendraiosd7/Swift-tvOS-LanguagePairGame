c3f3e53b6ef89dbba97df96ad930d09781fc916f
Narendra <narendra.vadde@zee.esselgroup.com>
2020-10-23 12:52:15 +0530

    added code

---
+++ Swift-tvOS-LanguagePairs/GameCard.swift
@@ -0,0 +1,13 @@
+//
+//  GameCard.swift
+//  Swift-tvOS-LanguagePairs
+//
+//  Created by narendra. vadde on 23/10/20.
+//  Copyright © 2020 narendra. vadde. All rights reserved.
+//
+
+import UIKit
+
+class GameCard: UICollectionViewCell {
+    
+}
---
+++ Swift-tvOS-LanguagePairs/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Swift-tvOS-LanguagePairs
+//
+//  Created by narendra. vadde on 23/10/20.
+//  Copyright © 2020 narendra. vadde. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
