8045863d8b3761b8ac239848c7316853cf437020
Narendra <narendra.vadde@zee.esselgroup.com>
2020-10-23 09:47:17 +0530

    Initial Commit

---
+++ Swift-tvOS-LanguagePairs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-tvOS-LanguagePairs
+//
+//  Created by narendra. vadde on 23/10/20.
+//  Copyright © 2020 narendra. vadde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
